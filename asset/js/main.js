6f4803345049674270fd8851dad9e4b6b74f5c87
yudantoardi <yudantoardi@gmail.com>
2022-11-21 16:07:03 +0700

    updates

---
+++ asset/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    var yourNavigation = $("header");
+        stickyDiv = "scrolled";
+
+    $(window).scroll(function() {
+    if( $(this).scrollTop() > 0 ) {
+        yourNavigation.addClass(stickyDiv);
+    } else {
+        yourNavigation.removeClass(stickyDiv);
+    }
+    });
+
+    $(".mob-toggle-bt").click(function(){
+        $(".dropdown").removeClass("show");
+        $(this).next().addClass("show");
+    });
+
+    $(".burger").click(function(){
+        $($(this).children("i")).toggleClass("fa-times fa-bars")
+        $(".menu").toggleClass("show");
+    });
+});
